58cb642e72317ee2ac843e7137ac6babe36c36a7
Binit Ghosh <www.binit108walla@gmail.com>
2024-12-09 09:53:30 +0530

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,23 @@
+const minusButton = document.getElementById('minus');
+const plusButton = document.getElementById('plus');
+const inputField = document.getElementById('input');
+
+minusButton.addEventListener('click', event => {
+  event.preventDefault();
+  
+  const currentValue = Number(inputField.value) || 0;
+  inputField.value = currentValue - 1;
+
+});
+
+plusButton.addEventListener('click', event => {
+  event.preventDefault();
+  const currentValue = Number(inputField.value) || 0;
+  inputField.value = currentValue + 1;
+});
+
+
+function clickimg(smallimg) {
+  var fullimg = document.getElementById("mainImage"); 
+  fullimg.src = smallimg.src;
+}
